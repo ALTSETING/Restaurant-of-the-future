8e2f078ae8c1cfa9a31602c97b434f146305f5fd
ALTSETING <vladislav.melnyk.kripto@gmail.com>
2026-02-16 18:02:57 +0100

    Add Optional type to imports in main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
-from typing import List, Literal, Dict
+from typing import List, Literal, Dict, Optional
 from datetime import datetime
 from pathlib import Path
 
